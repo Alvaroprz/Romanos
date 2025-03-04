e6cccf05ea2e73e03f1b24baedd91853f0ede778
Alvaroprz <alvaroperezsaez7@hotmail.com>
2025-03-04 19:58:22 +0100

    Enunciado y validación de la entrada

---
+++ romanos.py
@@ -0,0 +1,12 @@
+def convertir_a_romano(numero):
+    
+    
+
+    if type(numero) is not int:
+        return "ERROR: el número debe ser un entero"
+    
+    if not (0 < numero < 4000):
+        return "ERROR: número debe estar entre 1 y 3999"
+    
+    
+    return ""
