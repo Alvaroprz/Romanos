81b64e5be5741ed7b0aaaf39ed197a15200caade
Alvaroprz <alvaroperezsaez7@hotmail.com>
2025-03-04 21:37:41 +0100

    refactorizamos para meter en un bucle

--- romanos.py
+++ romanos.py
@@ -1,16 +1,23 @@
 def convertir_a_romano(numero):
     
-    
-
     if type(numero) is not int:
         return "ERROR: el número debe ser un entero"
     
     if not (0 < numero < 4000):
         return "ERROR: número debe estar entre 1 y 3999"
     
-    romano = " "
+    millares = ["", "M", "MM", "MMM"]
+    centenas = [" ","C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    unidades = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    decenas = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
 
-    cociente = numero // 1000
-    romano = romano + "M" * cociente
+    conversores = [unidades, decenas, centenas , millares]
+
+    romano = " "
+    
+    for n in range(3, -1, -1):
+        cociente = numero // 10 ** n
+        romano = romano + conversores[n][cociente]
+        numero = numero % 10 ** n
 
     return romano
